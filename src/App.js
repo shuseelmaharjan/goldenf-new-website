12cc450ee4f6a4167033a24919758a8fa2ead19c
shuseelmaharjan <maharzanshuseel10@gmail.com>
2024-08-20 20:31:03 +0545

    first commit with change in route with custom layout using switch statement

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import './App.css';
-import { BrowserRouter as Router, Routes, Route, useLocation, Navigate } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/Home/Home';
-import Navbar from './components/Navbar/Navbar';
 import Courses from './components/Courses/Courses';
 import Languages from './components/Languages/Languages';
 import Tuition from './components/Tuition/Tuition';
@@ -10,42 +9,10 @@ import Events from './components/Events/Events';
 import Syllabus from './components/Syllabus/Syllabus';
 import Contact from './components/Contact/Contact';
 import CoursesDetails from './components/Slug/CoursesDetails';
-import Footer from './components/Footer/Footer';
 import Login from './components/Login/Login';
 import Dashboard from './components/Dashboard/Dashboard';
-import Sidebar from './components/Navbar/Sidebar';
 import Exam from './components/Exams/Exam';
-
-function Layout({ children }) {
-  const location = useLocation();
-  const isLogin = location.pathname === '/login';
-  const isDashboardOrExam = location.pathname === '/dashboard' || location.pathname === '/exam';
-
-  return (
-    <>
-      {!isLogin && !isDashboardOrExam && (
-        <>
-          <Navbar />
-          <Footer />
-        </>
-      )}
-
-      {isDashboardOrExam && <Sidebar />}
-
-      <div>{children}</div>
-    </>
-  );
-}
-
-function PrivateRoute({ children }) {
-  const token = localStorage.getItem('token');
-
-  if (!token) {
-    return <Navigate to="/login" />;
-  }
-
-  return children;
-}
+import Layout from './Layout';
 
 function App() {
   return (
@@ -62,8 +29,8 @@ function App() {
           <Route path='/syllabus' element={<Syllabus />} />
           <Route path='/contact' element={<Contact />} />
           <Route path='/login' element={<Login />} />
-          <Route path='/dashboard' element={<PrivateRoute><Dashboard /></PrivateRoute>} />
-          <Route path='/exam' element={<PrivateRoute><Exam /></PrivateRoute>} />
+          <Route path='/dashboard' element={<Dashboard />} />
+          <Route path='/exam' element={<Exam />} />
         </Routes>
       </Layout>
     </Router>
